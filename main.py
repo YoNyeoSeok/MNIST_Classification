ad1e4a1d9ab3701616829865062d2785cb36ac39
YoNyeoSeok <yys8646@postech.ac.kr>
2019-03-01 16:33:27 +0900

    main

--- main.py
+++ main.py
@@ -19,55 +19,38 @@ assert len(y_train_one_hot.shape) == 2
 get_train_batch = get_batch(X_train)
 
 # model
-import tensorflow as tf
-tf_X = tf.placeholder(tf.float32, shape=[None, feature], name='X')
-tf_y = tf.placeholder(tf.float32, shape=[None, n_class], name='y')
+from model.tensorflow.logistic import LOGISTIC
+model = LOGISTIC(input_dim=feature, output_dim=n_class)
+model.train_setting(optimizer='Adam')
+model.model_init()
 
-w_init = np.random.randn(feature, n_class).astype(np.float32) / np.sqrt(feature)
-w = tf.get_variable("w", initializer = tf.constant(w_init))
-b = tf.get_variable("b", initializer = tf.zeros(n_class))
-y_pred = tf_X @ w + b
-
-confusion_matrix = tf.confusion_matrix(labels=tf.argmax(tf_y, 1), 
-        predictions=tf.argmax(y_pred, 1))
- 
-losses = tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf_y, logits=y_pred)
-loss = tf.reduce_mean(losses)
-correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(tf_y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-opt = tf.train.AdamOptimizer(.001, .9, .999)
-step = opt.minimize(loss)
-
-# train, log
+# train, logs
 training_logs = {'loss':[[] for _ in range(5)], 'accuracy':[[] for _ in range(5)]}
 validation_logs = {'loss':[[] for _ in range(5)], 'accuracy':[[] for _ in range(5)]}
 test_logs = {'loss':[[] for _ in range(5)], 'accuracy':[[] for _ in range(5)]}
 for j in range(5):
-    init = tf.global_variables_initializer()
-    with tf.Session() as sess:
-        sess.run(init)
-        for i in range(500+1):
-            batch_idxs = get_train_batch.get_batch_idxs()
-            outputs = sess.run([loss, accuracy, step], 
-                    feed_dict={tf_X:X_train[batch_idxs], tf_y:y_train_one_hot[batch_idxs]})
-            training_logs['loss'][j].append(outputs[0])
-            training_logs['accuracy'][j].append(outputs[1])
-            
-            if i % 10 == 0:
-                outputs = sess.run([loss, accuracy], 
-                        feed_dict={tf_X:X_test, tf_y:y_test_one_hot})
-                validation_logs['loss'][j].append(outputs[0])
-                validation_logs['accuracy'][j].append(outputs[1])
-    
-        outputs = sess.run([loss, accuracy, confusion_matrix],
-            feed_dict={tf_X:X_test, tf_y:y_test_one_hot})
-        test_logs['loss'][j].append(outputs[0])
-        test_logs['accuracy'][j].append(outputs[1])
-        test_logs.update({'compusion_matrix': outputs[2]})
-        print('compusion_matrix:\n', outputs[2])
-
-logs = {'training_logs':training_logs, 'validation_logs':validation_logs, 'test_logs':test_logs, 'validation_interval':10}
+    model.model_init()
+    for i in range(500+1):
+        batch_idxs = get_train_batch.get_batch_idxs()
+        model.train_step(x=X_train[batch_idxs], y=y_train_one_hot[batch_idxs])
+        outputs = model.forward(x=X_train[batch_idxs], y=y_train_one_hot[batch_idxs])
+        training_logs['loss'][j].append(outputs[1])
+        training_logs['accuracy'][j].append(outputs[2])
+        
+        if i % 10 == 0:
+            outputs = model.forward(x=X_test, y=y_test_one_hot)
+            validation_logs['loss'][j].append(outputs[1])
+            validation_logs['accuracy'][j].append(outputs[2])
+    outputs = model.forward(x=X_test, y=y_test_one_hot)
+    test_logs['loss'][j].append(outputs[1])
+    test_logs['accuracy'][j].append(outputs[2])
+    test_logs.update({'compusion_matrix': outputs[3]})
+    print('test loss, test accuracy', outputs[1], outputs[2])
+    print('compusion matrix:\n', outputs[3])
+logs = {'training_logs':training_logs, 
+        'validation_logs':validation_logs, 
+        'test_logs':test_logs, 
+        'validation_interval':10}
 np.save('logs', logs)
-
+            
 # model save
--- model/tensorflow/logistic.py
+++ model/tensorflow/logistic.py
@@ -7,16 +7,16 @@ class LOGISTIC():
         self.output_dim = output_dim
        
         w_init = np.random.randn(input_dim, output_dim) / np.sqrt(input_dim)
-        self.w = tf.get_variable('w',
-                tf.constant(w_init))
-        self.b = tf.get_variable('b',
-                tf.zeros(output_dim))
+        self.w = tf.get_variable('w', dtype=tf.float32,
+                initializer = tf.cast(tf.constant(w_init), "float"))
+        self.b = tf.get_variable('b', dtype=tf.float32,
+                initializer = tf.zeros(output_dim))
 
         self.inp = tf.placeholder(tf.float32, [None, input_dim], 'inp')
         self.target = tf.placeholder(tf.float32, [None, output_dim], 'target')
         
         self.logits = self.inp @ self.w + self.b
-        self.pred = tf.softmax(self.logits, 1)
+        self.pred = tf.nn.softmax(self.logits, 1)
 
         self.loss = tf.reduce_mean(
                 tf.nn.softmax_cross_entropy_with_logits_v2(labels = self.target, 
@@ -25,7 +25,7 @@ class LOGISTIC():
         self.correct_pred = tf.equal(
                 tf.argmax(self.pred, 1), tf.argmax(self.target, 1))
         self.accuracy = tf.reduce_mean(
-                tf.cast(tf.correct_pred, "float"))
+                tf.cast(self.correct_pred, "float"))
 
         self.confusion_matrix = tf.confusion_matrix(labels = tf.argmax(self.target, 1),
                 predictions = tf.argmax(self.pred, 1))
@@ -34,20 +34,30 @@ class LOGISTIC():
         self.sess.run(tf.global_variables_initializer())
 
         self.optim = None
+        self.model_init()
 
-    def model_setting(self, optim=tf.nn.AdamOptimizer()):
-        self.optim = optim
+    def model_init(self):
+        self.sess.run(tf.global_variables_initializer())
+
+    def train_setting(self, optimizer='Adam'):
+        if optimizer == 'Adam':
+            self.optim = tf.train.AdamOptimizer()
         self.step = self.optim.minimize(self.loss)
         
     def train_step(self, x, y):
         outputs = [self.step]
         return self.sess.run(outputs, feed_dict={self.inp:x, self.target:y})
 
-    def forward(self, x):
+    def forward(self, x, y=None):
         if len(x.shape) != 2:
             x = x.reshape(-1, np.prod(x.shape[1:]))
         # assert len(x.shape) == 2, x.shape
         assert x.shape[1] == self.input_dim, x.shape
 
-        return sess.run([self.pred], feed_dict={self.inp: x})
+        outputs = [self.pred]
+        feed_dict = {self.inp:x}
+        if y is not None:
+            outputs += [self.loss, self.accuracy, self.confusion_matrix]
+            feed_dict.update({self.target:y})
+        return self.sess.run(outputs, feed_dict=feed_dict)
     
